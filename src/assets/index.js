d9f71efa2f9763cc0cc5d3033e30b009a3317968
Udeorah Frank <franklynudeorah@gmail.com>
2023-08-15 08:44:37 +0100

    add images for banner slider

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,7 @@
 import logoText from './logo-text.svg';
 import cart from './cart.svg';
+import img1 from './img1.jpg';
+import img2 from './img2.jpg';
+import img4 from './img4.jpg';
 
-export { logoText, cart }
+export { logoText, cart, img1, img2, img4 }

4ef30666d95aa4f7c008d44cb40ffe8ee619e5be
Udeorah Frank <franklynudeorah@gmail.com>
2023-08-20 12:01:05 +0100

    create category card component and refactor categories ection layout

---
+++ src/components/CartegoryCard.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({
+  title: String
+})
+</script>
+
+<template>
+  <div class="px-2 py-3">
+    <router-link :to="{name: 'home'}">
+      <div class="img-container bg-[#F3F3F3] p-6 w-32 h-auto md:w-full">
+        <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="" class="w-full h-full lg:w-[80%] mx-auto mix-blend-darken duration-500">
+      </div>
+      <p class="py-3 font-semibold">{{ title }}</p>
+    </router-link>
+  </div>
+</template>
+
+<style scoped>
+.img-container:hover img {
+  transform: scale(1.05);
+}
+</style>

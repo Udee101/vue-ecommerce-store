4b72af334ae8a08d1a8f93af68d8827d5aedc7fe
Udeorah Frank <franklynudeorah@gmail.com>
2023-08-21 02:42:33 +0100

    modify product card component

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,23 +1,31 @@
 <script setup>
-	defineProps({
+import { computed } from 'vue';
+
+	const props = defineProps({
 		product: Object,
+    imgContainerWidth: String
 	});
+
+  const formatTitle = computed(() => {
+    let text = props.product.title.slice(0, 16)
+    return `${text}...`
+  })
 </script>
 
 <template>
-	<div class="py-3 md:w-[30%]">
+	<div class="w-full">
 		<router-link :to="{ name: 'home' }">
-			<div class="img-container bg-grey p-6 w-44 h-[72%] md:w-full">
+			<div :class="`img-container bg-grey p-6 ${imgContainerWidth} h-[72%] md:w-full`">
 				<img
 					:src="product.image"
 					alt=""
 					class="w-full h-full lg:w-[80%] mx-auto mix-blend-darken duration-500"
 				/>
 			</div>
-			<p class="pt-2 text-sm font-semibold w-full text-left">{{ product.title }}</p>
-			<p class="text-sm font-light w-full text-left">
-				&dollar;{{ product.price }}
-			</p>
+      <p class="pt-2 text-sm font-semibold w-full text-left">{{ formatTitle }}</p>
+      <p class="text-sm font-light w-full text-left">
+        &dollar;{{ product.price }}
+      </p>
 		</router-link>
 	</div>
 </template>

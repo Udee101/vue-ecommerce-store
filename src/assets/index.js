6e3fd3ea75ffe1fd9a481068cb02fd9816557f77
Udeorah Frank <franklynudeorah@gmail.com>
2023-10-02 22:51:27 +0100

    create the cart page

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,6 @@
 import logoText from './logo-text.svg';
-import cart from './cart.svg';
 import img1 from './img1.jpg';
 import img2 from './img2.jpg';
 import img4 from './img4.jpg';
 
-export { logoText, cart, img1, img2, img4 }
+export { logoText, img1, img2, img4 }

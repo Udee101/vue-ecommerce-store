b49282fae837b3df10e2d522abc4b1221dcf0457
Udeorah Frank <franklynudeorah@gmail.com>
2023-10-14 13:33:54 +0100

    create navigation guard

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,4 +6,36 @@ const router = createRouter({
   routes,
 })
 
+const invalidateToken = () => {
+  if (localStorage.getItem("token_creation_time") === null) {
+    localStorage.removeItem("user_token");
+  }
+
+  const createdAt = parseInt(localStorage.getItem("token_creation_time"));
+  const elapsedTimeInSeconds = (Date.now() - createdAt) / 1000;
+
+  if (elapsedTimeInSeconds > 600) {
+    localStorage.removeItem("user_token");
+  }
+}
+
+router.beforeEach((to, from, next) => {
+
+  invalidateToken();
+
+  const authenticated = localStorage.getItem("user_token");
+
+  if (to.matched.some((record) => record.meta.guest)) {
+    if (authenticated) {
+      next({
+        name: 'home',
+      })
+    } else {
+      next()  
+    }
+  } else {
+    next()
+  }
+})
+
 export default router;

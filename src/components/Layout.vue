00f3b705322a6ac6dc0a7871aa1e4e24832180d1
Udeorah Frank <franklynudeorah@gmail.com>
2023-12-13 00:00:10 +0100

    remove navbar from login and register page

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,15 +1,26 @@
 <script setup>
+import { computed } from 'vue';
 import Footer from './global/Footer.vue';
 import NavBar from './global/NavBar.vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const isNoNavRoute = computed(() => {
+  return [ "login", "register" ].includes(route.name)
+})
 
 </script>
 
 <template>
   <div>
-    <NavBar />
+    <template v-if="!isNoNavRoute">
+      <NavBar />
+    </template>
     <main class="pt-4 pb-[30rem] md:pb-80 min-h-screen ">
       <slot />
     </main>
-    <Footer />
+    <template v-if="!isNoNavRoute">
+      <Footer />
+    </template>
   </div>
 </template>
